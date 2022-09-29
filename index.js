6bb2e11e4063b4578dbac92a08ef2db660fba84c
HenTK <thachkimhen2000@gmail.com>
2022-09-29 21:13:38 +0700

    JS DayOne HomeWork

---
+++ index.js
@@ -0,0 +1,29 @@
+// HomeWork 01: Staff's Salary
+var SalaryPerDay = 100000;
+var inputWorkingDays = 29;
+var totalIncome = SalaryPerDay * inputWorkingDays;
+console.log("HomeWork 01, Total income: ", totalIncome);
+// HomeWork 02: Average 5 Real Nums
+var a = 1.0;
+var b = 2.0;
+var c = 3.0;
+var d = 4.0;
+var e = 5.5;
+var averageVal = (a + b + c + d + e) / 5.0;
+console.log("HomeWork 02, Average of a, b, c, d, e: ", averageVal);
+// HomeWork 03: Currency Exchange
+var USD = 2;
+var USDtoVND = 23500;
+var VND = USD * USDtoVND;
+console.log("Home Work 03, USD to Dollar: ", VND);
+// HomeWork 04: Area and Perimeter
+var f = 5;
+var g = 3;
+var Area = f * g;
+var Perimeter = (f + g) * 2;
+console.log("HomeWork 03, Area: ", Area, "; Perimeter: ", Perimeter);
+// HomeWork 05: Sum
+var inputNum = 15;
+var h = Math.floor(inputNum / 10);
+var i = inputNum % 10;
+console.log("HomeWork 05, Sum: ", h + i);
